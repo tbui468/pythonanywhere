596e8e1ef7c1aab9e2fc5135171cf0ef6ebb457d
thomas <thomasthienbui@gmail.com>
2024-11-09 08:03:01 +0000

    my first commit

---
+++ new_name.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+db = sqlite3.connect("./../blog.db")
+
+db.execute("DROP TABLE IF EXISTS users")
+db.execute("CREATE TABLE users (username TEXT, password TEXT)")
+
+db.execute("INSERT INTO users (username, password) VALUES ('Jim', 'jimpassword')")
+db.execute("INSERT INTO users (username, password) VALUES ('Kate', 'jimpassword')")
+db.commit()
+db.close()
+
+
